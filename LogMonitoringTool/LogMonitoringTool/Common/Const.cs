1ef292d37f5d0e09b97b170892af681765c3c4a5
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-03-23 19:46:43 +0900

    定数ファイル作成と固定文言の定義

---
+++ LogMonitoringTool/LogMonitoringTool/Common/Const.cs
@@ -0,0 +1,187 @@
+﻿namespace LogMonitoringTool.Common {
+
+	/// <summary>
+	/// 定数定義クラス
+	/// </summary>
+	public class Const {
+
+		/// <summary>
+		/// 固定文言
+		/// </summary>
+		public class FixedWording {
+
+			/// <summary>
+			/// MainWindow
+			/// </summary>
+			public class MainWindow {
+
+				/// <summary>
+				/// タイトル
+				/// </summary>
+				public const string Title = "ログ監視ツール";
+
+				/// <summary>
+				/// ログファイル参照ボタン
+				/// </summary>
+				public const string BrowseLogFileButton = "ログファイル参照";
+
+				/// <summary>
+				/// ログ内容確認ボタン
+				/// </summary>
+				public const string ConfirmLogButton = "ログ内容確認";
+
+				/// <summary>
+				/// 解析項目一覧表示ボタン
+				/// </summary>
+				public const string DisplayAnalysisListButton = "解析項目一覧表示";
+
+				/// <summary>
+				/// ログ解析開始ボタン
+				/// </summary>
+				public const string StartLogAnalysisButton = "ログ解析開始";
+
+				/// <summary>
+				/// 閉じるボタン
+				/// </summary>
+				public const string CloseButton = "閉じる";
+
+			}
+
+			/// <summary>
+			/// ConfirmationWindow
+			/// </summary>
+			public class ConfirmationWindow {
+
+				/// <summary>
+				/// タイトル
+				/// </summary>
+				public const string Title = "ログ内容確認";
+
+				/// <summary>
+				/// 閉じるボタン
+				/// </summary>
+				public const string CloseButton = "閉じる";
+
+			}
+
+			/// <summary>
+			/// AnalysisEditWindow
+			/// </summary>
+			public class AnalysisEditWindow {
+
+				/// <summary>
+				/// 新規作成時のタイトル
+				/// </summary>
+				public const string TitleWhenNewlyCreated = "解析項目新規作成";
+
+				/// <summary>
+				/// 編集時のタイトル
+				/// </summary>
+				public const string TitleWhenEditing = "解析項目編集";
+
+				/// <summary>
+				/// タイトルラベル
+				/// </summary>
+				public const string TitleLabel = "タイトル";
+
+				/// <summary>
+				/// 危険度ラベル
+				/// </summary>
+				public const string LiskLabel = "危険度";
+
+				/// <summary>
+				/// 正規表現ラベル
+				/// </summary>
+				public const string RegularExpressionLabel = "正規表現";
+				
+				/// <summary>
+				/// 説明ラベル
+				/// </summary>
+				public const string DescriptionLabel = "説明";
+
+				/// <summary>
+				/// 新規作成時の決定ボタン
+				/// </summary>
+				public const string DecisionWhenNewlyCreatedButton = "追加";
+
+				/// <summary>
+				/// 編集時の決定ボタン
+				/// </summary>
+				public const string DecisionWhenEditingButton = "編集";
+				
+				/// <summary>
+				/// キャンセルボタン
+				/// </summary>
+				public const string CancelButton = "キャンセル";
+
+			}
+
+			/// <summary>
+			/// AnalysisListWindow
+			/// </summary>
+			public class AnalysisListWindow {
+
+				/// <summary>
+				/// タイトル
+				/// </summary>
+				public const string TitleWhenNewlyCreated = "解析項目一覧";
+
+				/// <summary>
+				/// タイトルヘッダー
+				/// </summary>
+				public const string TitleHeader = "タイトル";
+
+				/// <summary>
+				/// 危険度ヘッダー
+				/// </summary>
+				public const string LiskHeader = "危険度";
+
+				/// <summary>
+				/// 正規表現ヘッダー
+				/// </summary>
+				public const string RegularExpressionHeader = "正規表現";
+
+				/// <summary>
+				/// 新規作成ボタン
+				/// </summary>
+				public const string NewCreationButton = "新規作成";
+
+				/// <summary>
+				/// 編集ボタン
+				/// </summary>
+				public const string EditButton = "編集";
+
+				/// <summary>
+				/// 削除ボタン
+				/// </summary>
+				public const string DeleteButton = "削除";
+
+				/// <summary>
+				/// 閉じるボタン
+				/// </summary>
+				public const string CloseButton = "閉じる";
+
+			}
+
+			/// <summary>
+			/// ResultWindow
+			/// </summary>
+			public class ResultWindow {
+
+				/// <summary>
+				/// タイトル
+				/// </summary>
+				public const string Title = "ログ解析結果";
+
+				/// <summary>
+				/// 閉じるボタン
+				/// </summary>
+				public const string CloseButton = "閉じる";
+
+			}
+
+		}
+
+	}
+
+}
