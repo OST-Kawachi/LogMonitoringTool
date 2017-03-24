bd6c7a86d12f8deb6104d29f18105cb13b5cf875
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-03-24 16:36:00 +0900

    アプリ終了コマンド実装

--- LogMonitoringTool/LogMonitoringTool/ViewModels/Main/MainViewModel.cs
+++ LogMonitoringTool/LogMonitoringTool/ViewModels/Main/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using LogMonitoringTool.Common;
+﻿using LogMonitoringTool.Command.AppManipulation;
+using LogMonitoringTool.Common;
+using System.Windows.Input;
 
 namespace LogMonitoringTool.ViewModels.Main {
 
@@ -13,11 +15,54 @@ namespace LogMonitoringTool.ViewModels.Main {
 		public string TitleText { get; }
 
 		/// <summary>
+		/// 選択したログファイル
+		/// </summary>
+		public string SelectedLogFileName { set; get; }
+
+		/// <summary>
+		/// ログファイル参照ボタン表示文字
+		/// </summary>
+		public string BrowseLogFileButtonContent { get; }
+
+		/// <summary>
+		/// ログ内容確認ボタン表示文字
+		/// </summary>
+		public string ConfirmLogButtonContent { get; }
+
+		/// <summary>
+		/// 解析項目一覧表示ボタン表示文字
+		/// </summary>
+		public string DisplayAnalysisListButtonContent { get; }
+
+		/// <summary>
+		/// ログ解析開始ボタン
+		/// </summary>
+		public string StartLogAnalysisButtonContent { get; }
+
+		/// <summary>
+		/// 閉じるボタン
+		/// </summary>
+		public string CloseButtonContent { get; }
+
+		/// <summary>
+		/// アプリ終了コマンド
+		/// </summary>
+		public ApplicationCloseCommand ApplicationCloseCommand { get; }
+
+		/// <summary>
 		/// コンストラクタ
 		/// </summary>
 		public MainViewModel() {
 
 			this.TitleText = Const.FixedWording.MainWindow.Title;
+			this.SelectedLogFileName = "";
+			this.BrowseLogFileButtonContent = Const.FixedWording.MainWindow.BrowseLogFileButton;
+			this.ConfirmLogButtonContent = Const.FixedWording.MainWindow.ConfirmLogButton;
+			this.DisplayAnalysisListButtonContent = Const.FixedWording.MainWindow.DisplayAnalysisListButton;
+			this.StartLogAnalysisButtonContent = Const.FixedWording.MainWindow.StartLogAnalysisButton;
+			this.CloseButtonContent = Const.FixedWording.MainWindow.CloseButton;
+
+			this.ApplicationCloseCommand = new ApplicationCloseCommand();
 
 		}
 
