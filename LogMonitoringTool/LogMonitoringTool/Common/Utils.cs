596a4e30e61aee9bd81a025c201d0e3cde6a243e
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-04-03 16:53:49 +0900

    共通処理クラス作成　ファイルパスからファイル内容を取得するクラス作成

---
+++ LogMonitoringTool/LogMonitoringTool/Common/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace LogMonitoringTool.Common {
+
+	/// <summary>
+	/// 共通処理
+	/// </summary>
+	class Utils {
+
+		/// <summary>
+		/// ファイルパスからファイル内のテキストを取得する
+		/// </summary>
+		/// <param name="filePath">ファイルパス</param>
+		/// <returns>ファイル内のテキスト</returns>
+		public static string GetTextOfFile( string filePath ) {
+
+			string resultText = "";
+
+			try {
+
+				//ファイルを開く
+				using( StreamReader sr = new StreamReader( filePath , Encoding.GetEncoding( "Shift_JIS" ) ) ) {
+
+					string text = "";
+					string line;
+					while( ( line = sr.ReadLine() ) != null ) {
+						text += line + Environment.NewLine;
+					}
+					resultText = text;
+
+				}
+
+			}
+			catch( Exception ) {
+				MessageBox.Show( "ログファイルが開けませんでした。\nShiftJISのtxtファイルとして保存しなおし、再度確認してください。" , "エラー" , MessageBoxButton.OK , MessageBoxImage.Error );
+			}
+
+			return resultText;
+
+		}
+
+	}
+
+}
