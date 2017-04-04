f73ee410fba805d6403cc9564663f07888f07424
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-04-04 20:00:21 +0900

    XML読み書きクラス作成

---
+++ LogMonitoringTool/LogMonitoringTool/Model/XmlSerialization/AnalysisDataXmlModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace LogMonitoringTool.Model.XmlSerialization {
+
+	/// <summary>
+	/// AnalysisData.xmlのモデル
+	/// </summary>
+	[ XmlRoot( "root" ) ]
+	public class AnalysisDataXmlModel {
+
+		/// <summary>
+		/// item
+		/// </summary>
+		public class ItemModel {
+
+			/// <summary>
+			/// 連番
+			/// </summary>
+			[ XmlAttribute( "no" ) ]
+			public int no;
+
+			/// <summary>
+			/// タイトル
+			/// </summary>
+			[ XmlAttribute( "title" ) ]
+			public string title;
+
+			/// <summary>
+			/// 危険度
+			/// </summary>
+			[ XmlAttribute( "risk" ) ]
+			public string risk;
+
+			/// <summary>
+			/// 正規表現
+			/// </summary>
+			[ XmlAttribute( "regex" ) ]
+			public string regex;
+
+			/// <summary>
+			/// 詳細
+			/// </summary>
+			[ XmlAttribute( "info" ) ]
+			public string info;
+
+		}
+
+		/// <summary>
+		/// 項目一覧
+		/// </summary>
+		[ XmlArray( "items" ) , XmlArrayItem( "item" ) ]
+		public List<ItemModel> items = new List<ItemModel>();
+		
+	}
+
+}
---
+++ LogMonitoringTool/LogMonitoringTool/Services/XmlSerialization/XmlSerializationService.cs
@@ -0,0 +1,65 @@
+﻿using LogMonitoringTool.Model.XmlSerialization;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace LogMonitoringTool.Services.XmlSerialization {
+
+	/// <summary>
+	/// XMLの読み書きを行うServiceクラス
+	/// </summary>
+	public class XmlSerializationService {
+
+		/// <summary>
+		/// XML Model
+		/// </summary>
+		public AnalysisDataXmlModel model;
+
+		/// <summary>
+		/// シングルトン
+		/// </summary>
+		private static XmlSerializationService singleton = new XmlSerializationService();
+
+		/// <summary>
+		/// ファイルパス
+		/// </summary>
+		private const string FileName = @"\\Data\AnalysisFile\AnalysisData.xml";
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		private XmlSerializationService() {
+
+			try {
+				using( FileStream fileStream = new FileStream( FileName , FileMode.Open ) ) {
+					XmlSerializer serializer = new XmlSerializer( typeof( AnalysisDataXmlModel ) );
+					this.model = (AnalysisDataXmlModel)serializer.Deserialize( fileStream );
+				}
+			}
+			catch( Exception ) { }
+
+		}
+
+		/// <summary>
+		/// インスタンスを返す
+		/// </summary>
+		/// <returns></returns>
+		public static XmlSerializationService GetInstance() {
+			return singleton;
+		}
+
+		/// <summary>
+		/// 今のXML ModelをXMLファイルに書き込む
+		/// </summary>
+		public void Write() {
+
+			using( FileStream fileStream = new FileStream( FileName , FileMode.Create ) ) {
+				XmlSerializer serializer = new XmlSerializer( typeof( AnalysisDataXmlModel ) );
+				serializer.Serialize( fileStream , this.model );
+			}
+
+		}
+
+	}
+
+}
