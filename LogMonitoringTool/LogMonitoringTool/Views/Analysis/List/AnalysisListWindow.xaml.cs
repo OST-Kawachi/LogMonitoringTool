4eb8f6a33efcbb177ceca7f31b62145593fd36d8
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-04-04 20:09:30 +0900

    固定文言、イベント枠だけ作成

--- LogMonitoringTool/LogMonitoringTool/Views/Analysis/List/AnalysisListWindow.xaml.cs
+++ LogMonitoringTool/LogMonitoringTool/Views/Analysis/List/AnalysisListWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using LogMonitoringTool.Services.XmlSerialization;
+using System.Windows;
 
 namespace LogMonitoringTool.Views.AnalysisList {
 
@@ -7,12 +8,85 @@ namespace LogMonitoringTool.Views.AnalysisList {
 	/// </summary>
 	public partial class AnalysisListWindow : Window {
 
+		/// <summary>
+		/// 解析項目
+		/// </summary>
+		private class AnalysisListDataGridItem {
+
+			/// <summary>
+			/// タイトル
+			/// </summary>
+			public string Title { set; get; }
+
+			/// <summary>
+			/// リスク
+			/// </summary>
+			public string LiskLevel { set; get; }
+
+			/// <summary>
+			/// 正規表現
+			/// </summary>
+			public string RegularExpression { set; get; }
+
+			/// <summary>
+			/// 詳細
+			/// </summary>
+			public string Detail { set; get; }
+
+		}
+
+		/// <summary>
+		/// XML読み書きService
+		/// </summary>
+		XmlSerializationService service;
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public AnalysisListWindow() {
 
 			InitializeComponent();
 
+			this.service = XmlSerializationService.GetInstance();
+
+			this.AnalysisListDataGrid.ItemsSource = new[] {
+				new AnalysisListDataGridItem() { Title = "タイトル" , LiskLevel = "危険度" , RegularExpression = "正規表現" , Detail = "詳細" } ,
+				new AnalysisListDataGridItem() { Title = "タイトル" , LiskLevel = "危険度" , RegularExpression = "正規表現" , Detail = "詳細" } ,
+				new AnalysisListDataGridItem() { Title = "タイトル" , LiskLevel = "危険度" , RegularExpression = "正規表現" , Detail = "詳細" } ,
+				new AnalysisListDataGridItem() { Title = "タイトル" , LiskLevel = "危険度" , RegularExpression = "正規表現" , Detail = "詳細" } ,
+				new AnalysisListDataGridItem() { Title = "タイトル" , LiskLevel = "危険度" , RegularExpression = "正規表現" , Detail = "詳細" }
+			};
+
 		}
 
+		/// <summary>
+		/// 新規作成ボタン押下時イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnClickNewCreationButton( object sender , RoutedEventArgs e ) { }
+
+		/// <summary>
+		/// 編集ボタン押下時イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnClickEditButton( object sender , RoutedEventArgs e ) { }
+
+		/// <summary>
+		/// 削除ボタン押下時イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnClickDeleteButton( object sender , RoutedEventArgs e ) { }
+
+		/// <summary>
+		/// 閉じるボタン押下時イベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OnClickCloseButton( object sender , RoutedEventArgs e ) { }
+		
 	}
 
 }
