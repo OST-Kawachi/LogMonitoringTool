9709c95671c12c5504d032e9275d08ed0574c3d1
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-04-04 20:09:55 +0900

    ViewModelに固定文言だけ作成

--- LogMonitoringTool/LogMonitoringTool/ViewModels/Analysis/Edit/AnalysisEditViewModel.cs
+++ LogMonitoringTool/LogMonitoringTool/ViewModels/Analysis/Edit/AnalysisEditViewModel.cs
@@ -1,9 +1,57 @@
-﻿namespace LogMonitoringTool.ViewModels.Analysis.Edit {
+﻿using LogMonitoringTool.Common;
+
+namespace LogMonitoringTool.ViewModels.Analysis.Edit {
 
 	/// <summary>
 	/// AnalysisEditWindowのViewModel
 	/// </summary>
 	public class AnalysisEditViewModel {
+
+		/// <summary>
+		/// タイトル
+		/// </summary>
+		public string TitleText { get; }
+
+		/// <summary>
+		/// タイトルラベル
+		/// </summary>
+		public string TitleLabel { get; }
+
+		/// <summary>
+		/// 危険度ラベル
+		/// </summary>
+		public string LiskLabel { get; }
+
+		/// <summary>
+		/// 正規表現ラベル
+		/// </summary>
+		public string RegularExpressionLabel { get; }
+
+		/// <summary>
+		/// 決定ボタン
+		/// </summary>
+		public string DecisionButtonContent { get; }
+
+		/// <summary>
+		/// キャンセル
+		/// </summary>
+		public string CancelButton { get; }
+
+		/// <summary>
+		/// 説明ラベル
+		/// </summary>
+		public string DescriptionLabel { get; }
+
+		public AnalysisEditViewModel() {
+
+			this.TitleText = Const.FixedWording.AnalysisEditWindow.TitleWhenNewlyCreated;
+			this.TitleLabel = Const.FixedWording.AnalysisEditWindow.TitleLabel;
+			this.LiskLabel = Const.FixedWording.AnalysisEditWindow.LiskLabel;
+			this.RegularExpressionLabel = Const.FixedWording.AnalysisEditWindow.RegularExpressionLabel;
+			this.DecisionButtonContent = Const.FixedWording.AnalysisEditWindow.DecisionWhenNewlyCreatedButton;
+			this.CancelButton = Const.FixedWording.AnalysisEditWindow.CancelButton;
+			
+		}
 
 	}
 
