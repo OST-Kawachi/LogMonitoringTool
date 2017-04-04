c8205806425aeb2050326050a03ce407805933d0
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-04-04 20:12:42 +0900

    ViewModelに固定文言だけ作成

--- LogMonitoringTool/LogMonitoringTool/ViewModels/Result/ResultViewModel.cs
+++ LogMonitoringTool/LogMonitoringTool/ViewModels/Result/ResultViewModel.cs
@@ -1,9 +1,28 @@
-﻿namespace LogMonitoringTool.ViewModels.Result {
+﻿using LogMonitoringTool.Common;
+
+namespace LogMonitoringTool.ViewModels.Result {
 
 	/// <summary>
 	/// ResultWindowのViewModel
 	/// </summary>
 	public class ResultViewModel {
+
+		/// <summary>
+		/// タイトル
+		/// </summary>
+		public string TitleText { get; }
+
+		/// <summary>
+		/// 閉じるボタン
+		/// </summary>
+		public string CloseButtonContent { get; }
+
+		public ResultViewModel() {
+
+			this.TitleText = Const.FixedWording.ResultWindow.Title;
+			this.CloseButtonContent = Const.FixedWording.ResultWindow.CloseButton;
+
+		}
 
 	}
 
