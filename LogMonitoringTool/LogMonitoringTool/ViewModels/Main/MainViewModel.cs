a1fcf72ddaf31f88da2a1b6a867479ea1f8e7ee1
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-04-06 13:56:32 +0900

    region分け

--- LogMonitoringTool/LogMonitoringTool/ViewModels/Main/MainViewModel.cs
+++ LogMonitoringTool/LogMonitoringTool/ViewModels/Main/MainViewModel.cs
@@ -7,15 +7,12 @@ namespace LogMonitoringTool.ViewModels.Main {
 	/// </summary>
 	public class MainViewModel : ViewModelBase {
 
+		#region 固定文言
+
 		/// <summary>
 		/// タイトル
 		/// </summary>
 		public string TitleText { get; }
-
-		/// <summary>
-		/// 選択したログファイル
-		/// </summary>
-		public string SelectedLogFileName { set; get; }
 
 		/// <summary>
 		/// ログファイル参照ボタン表示文字
@@ -41,19 +38,31 @@ namespace LogMonitoringTool.ViewModels.Main {
 		/// 閉じるボタン
 		/// </summary>
 		public string CloseButtonContent { get; }
-		
+
+		#endregion
+
+		/// <summary>
+		/// 選択したログファイル
+		/// </summary>
+		public string SelectedLogFileName { set; get; }
+
 		/// <summary>
 		/// コンストラクタ
 		/// </summary>
 		public MainViewModel() {
 
+			#region 固定文言
+
 			this.TitleText = Const.FixedWording.MainWindow.Title;
-			this.SelectedLogFileName = "";
 			this.BrowseLogFileButtonContent = Const.FixedWording.MainWindow.BrowseLogFileButton;
 			this.ConfirmLogButtonContent = Const.FixedWording.MainWindow.ConfirmLogButton;
 			this.DisplayAnalysisListButtonContent = Const.FixedWording.MainWindow.DisplayAnalysisListButton;
 			this.StartLogAnalysisButtonContent = Const.FixedWording.MainWindow.StartLogAnalysisButton;
 			this.CloseButtonContent = Const.FixedWording.MainWindow.CloseButton;
+
+			#endregion
+
+			this.SelectedLogFileName = "";
 			
 		}
 
